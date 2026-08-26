6a08bec336b509aa08e114b04cf5ee6a5d067a51
Yuki Umetsu <yuuki.umetsu@gmail.com>
2026-08-26 09:28:29 -0500

    Export public perception contracts

--- src/cogniverse_framework/cognition/__init__.py
+++ src/cogniverse_framework/cognition/__init__.py
@@ -1,10 +1,13 @@
 """Environment-neutral cognitive coordination contracts."""
 
+from .perception import PerceptModality, PublicPercept
 from .state import CognitiveReference, CognitiveState, MemoryKind, ReferenceKind
 
 __all__ = [
     "CognitiveReference",
     "CognitiveState",
     "MemoryKind",
+    "PerceptModality",
+    "PublicPercept",
     "ReferenceKind",
 ]
