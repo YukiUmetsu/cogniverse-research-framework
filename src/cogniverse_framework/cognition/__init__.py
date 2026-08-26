08b79144066ad24c705f1b3ebcb0b6dbc1b2fcd9
Yuki Umetsu <yuuki.umetsu@gmail.com>
2026-08-26 03:23:52 -0500

    feat: export cognitive state contracts

---
+++ src/cogniverse_framework/cognition/__init__.py
@@ -0,0 +1,10 @@
+"""Environment-neutral cognitive coordination contracts."""
+
+from .state import CognitiveReference, CognitiveState, MemoryKind, ReferenceKind
+
+__all__ = [
+    "CognitiveReference",
+    "CognitiveState",
+    "MemoryKind",
+    "ReferenceKind",
+]
